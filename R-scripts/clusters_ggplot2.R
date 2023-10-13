693eed742263b2ccdf836f435a881cc169860481
Varada <Vardee.khot@gmail.com>
2023-10-12 22:58:00 -0600

    updated r scripts

--- R-scripts/clusters_ggplot2.R
+++ R-scripts/clusters_ggplot2.R
@@ -1,3 +1,6 @@
+# This script plots the clusters produced by Mfuzz in facets in a prettier way
+# To produce the clusters and get cluster memberships: Metatranscriptomics\R-scripts\mfuzz_clustering.R
+
 library(tidyverse)
 library(ComplexHeatmap)
 library(ggplot2)
@@ -11,6 +14,41 @@ colnames(all_data) <- c("geneid","cluster","0","12","24","30","36","40","44","48
 
 all_data <- as_tibble(all_data)
 
+all_data_m <- all_data %>%
+  replace(is.na(.),0) %>%
+  pivot_longer(c("0","12","24","30","36","40","44","48","52","56","60","66","68","72","78","84"),
+               names_to = "hour", values_to = "log2FC") %>%
+  group_by(cluster,hour) %>%
+  mutate(means = mean(log2FC))
+
+
+all_data_m$hour <- as.numeric(all_data_m$hour)
+
+p1 <- ggplot(all_data_m, aes(x=hour, y=log2FC, group=geneid))+
+  geom_hline(aes(yintercept = 0),linetype="dashed", lwd=0.75) +
+  geom_line(size=0.75,color="grey40", alpha=0.8)+
+  geom_smooth(data=all_data_m,aes(hour,means),color="royalblue", se=FALSE,size=2)+
+  geom_vline(aes(xintercept = 36),linetype="dotdash", lwd=0.75)+
+  geom_vline(aes(xintercept = 66),linetype="dotdash", lwd=0.75)+
+  scale_x_continuous(breaks=all_data_m$hour, labels=all_data_m$hour,guide = guide_axis(n.dodge = 2))+
+  facet_wrap(~all_data_m$cluster,
+             labeller = label_wrap_gen(width = 20), nrow = 3)+
+  labs(x="Hours", y="Log2 Fold Change")+
+  ylim(-10,10)+
+  theme(axis.text.y = element_text(colour = "black", size = 12,face="bold"), 
+        axis.text.x = element_text(colour = "black", size = 12), 
+        axis.title = element_text(face = "bold", size = 14, colour = "black"), 
+        legend.position = "none", 
+        panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2),
+        strip.background = element_blank(),strip.text = element_text(colour = "black", size = 12, face="bold"))
+
+p1
+library(svglite)
+ggsave("clusters_ggplot2.svg", plot=p1, width = 500, height = 300,units = "px",rast)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# to plot only 1 cluster
 
 subset_m <- all_data %>%
   filter(grepl("7", cluster, fixed = TRUE)) %>%
@@ -47,34 +85,3 @@ p_sub <- ggplot(subset_m_means, aes(x=hour, y=log2FC, group=geneid))+
 
 p_sub
 
-all_data_m <- all_data %>%
-  replace(is.na(.),0) %>%
-  pivot_longer(c("0","12","24","30","36","40","44","48","52","56","60","66","68","72","78","84"),
-               names_to = "hour", values_to = "log2FC") %>%
-  group_by(cluster,hour) %>%
-  mutate(means = mean(log2FC))
-
-
-all_data_m$hour <- as.numeric(all_data_m$hour)
-
-p1 <- ggplot(all_data_m, aes(x=hour, y=log2FC, group=geneid))+
-  geom_hline(aes(yintercept = 0),linetype="dashed", lwd=0.75) +
-  geom_line(size=0.75,color="grey40", alpha=0.8)+
-  geom_smooth(data=all_data_m,aes(hour,means),color="royalblue", se=FALSE,size=2)+
-  geom_vline(aes(xintercept = 36),linetype="dotdash", lwd=0.75)+
-  geom_vline(aes(xintercept = 66),linetype="dotdash", lwd=0.75)+
-  scale_x_continuous(breaks=all_data_m$hour, labels=all_data_m$hour,guide = guide_axis(n.dodge = 2))+
-  facet_wrap(~all_data_m$cluster,
-             labeller = label_wrap_gen(width = 20), nrow = 3)+
-  labs(x="Hours", y="Log2 Fold Change")+
-  ylim(-10,10)+
-  theme(axis.text.y = element_text(colour = "black", size = 12,face="bold"), 
-        axis.text.x = element_text(colour = "black", size = 12), 
-        axis.title = element_text(face = "bold", size = 14, colour = "black"), 
-        legend.position = "none", 
-        panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2),
-        strip.background = element_blank(),strip.text = element_text(colour = "black", size = 12, face="bold"))
-
-p1
-library(svglite)
-ggsave("clusters_ggplot2.svg", plot=p1, width = 500, height = 300,units = "px",rast)
--- R-scripts/mfuzz_clustering.R
+++ R-scripts/mfuzz_clustering.R
@@ -2,7 +2,7 @@
 # It uses log2 fold changes output by DESeq2, regardless of whether they are significant or not
 # fuzzifier "m" and number of clusters needs to be optimized
 # Plotting functionality of mfuzz is crap so it's better to ouput data from mfuzz and use ggplot to produce cluster graphs
-# For plotting clusters: Metatranscriptomics\R-scripts\clusters_ggplot2.R
+# For plotting clusters: \Metatranscriptomics\R-scripts\clusters_ggplot2.R
 # Refer to http://mfuzz.sysbiolab.eu/
 
 library(Mfuzz)
@@ -19,6 +19,9 @@ exp.set <- ExpressionSet(assayData = as.matrix(df))
 
 set.seed(123)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# FUZZIFIER AND CLUSTER NUMBER OPTIMIZATION
+
 # estimate fuzzifier "m"
 m1 <- mestimate(exp.set)
 
@@ -28,7 +31,9 @@ cselec <- cselection(exp.set,m=m1,crange=seq(4,50,2),repeats=5,visu=TRUE)
 #OR
 Dmin(exp.set,m1,crange=seq(4,50,2),repeats=3,visu=TRUE)
 
-# do the c-means clustering using multiple cluster numbers and m1 generated above
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# CHECK CLUSTER OVERLAP
+# do the c-means clustering using multiple cluster numbers and m1 generated above and check overlap of the clusters
 c18 <- mfuzz(exp.set,c=18,m=m1)
 c22 <- mfuzz(exp.set,c=22,m=m1)
 c30 <- mfuzz(exp.set,c=30,m=m1)
@@ -42,7 +47,8 @@ overlap.plot(exp.set,cl=c18,thres = 0.05,over=o18)
 overlap.plot(exp.set,cl=c30,thres = 0.05,over=o30)
 
 
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# PLOTTING CLUSTERS WITH DIFF NUMBERS
 
 mfuzz.plot2(exp.set,cl=c18,mfrow=c(4,5),x11=FALSE,
             ylim.set=c(-6,6), ax.col = "black", col = "purple",
@@ -50,7 +56,7 @@ mfuzz.plot2(exp.set,cl=c18,mfrow=c(4,5),x11=FALSE,
             time.labels=c(0,12,24,30,36,40,44,48,52,56,60,66,68,72,78,84),
             xlab = "Hours", ylab = "Log2 Fold Change");drawLines()
 
-drawLines <- function() abline(v=c(36,66),col="dodgerblue",lwd=2)
+drawLines <- function() abline(v=c(36,66),col="dodgerblue",lwd=2) # draws vertical lines
 
 
 mfuzz.plot2(exp.set,cl=c22,mfrow=c(5,5),x11=FALSE,
@@ -65,23 +71,28 @@ mfuzz.plot2(exp.set,cl=c30,mfrow=c(5,6),x11=FALSE,
 
 mfuzzColorBar(col="fancy",main="Membership",cex.main=1)
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# COMPARE WITH K-MEANS CLUSTERING
+
 k1 <- kmeans2(exp.set,k = 18,iter.max = 99)
 kmeans2.plot(exp.set,kl = k1,mfrow =c(4,5))
 k_df <- k1$cluster
 write.csv(k_df, "k_means_membership.csv")
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# WHAT GENES ARE IN WHAT CLUSTERS?
 #get membership of clusters
 set.seed(123)
-acore_clust <- acore(exp.set,cl=c18, min.acore=0.7)
+acore_clust <- acore(exp.set,cl=c18, min.acore=0.7) # set membership threshold 
 library(dplyr)
 library(purrr)
+library(tidyr)
 
 acore_clust %>%
   imap(function(x, y) x %>% rename_with(~paste(y, sep = '_'), -"NAME")) %>% 
   reduce(full_join, by = 'NAME') -> final_df
-
-library(tidyr)
 
 final_df_m <- melt()
 
@@ -89,9 +100,5 @@ final_df_m <- pivot_longer(final_df, names_to = "cluster", values_to = "membersh
                            cols = c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18"))
 final_df_m_na <- na.omit(final_df_m)
 
-
+# produces a csv file with clsuter memberships
 write.csv(final_df_m_na, "clusters_w_memship.csv")
-
-
-
-
